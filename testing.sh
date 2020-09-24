8842d6d6fdf86afae8f986941c2e2d9190ce40d5
vbence121 <vbence121@gmail.com>
2020-09-24 13:28:05 +0200

    Rewrite testing.sh

--- testing.sh
+++ testing.sh
@@ -11,12 +11,16 @@ for unit1 in units/*
 do
 	for unit2 in units/* 
   do
-    printf $unit1" "$unit2"\n"
-		if [ $unit1 != $unit2 ]
+	if [ $unit1 != $unit2 ]
     then
 			./a.out $unit1 $unit2 >> $FILE
 		fi
 	done
 done
 
-diff $FILE exp_output.txt
+DIFF=$(diff $FILE exp_output.txt) 
+if [ "$DIFF" != "" ] 
+then
+    echo "ERROR: there are diferences"
+	exit -1
+fi
