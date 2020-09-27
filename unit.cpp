974fcd6b7816a75f0e787a6b33ce8614e4053d51
vbence121 <vbence121@gmail.com>
2020-09-27 18:52:37 +0200

    Improved json handling in parseUnit function

--- unit.cpp
+++ unit.cpp
@@ -14,23 +14,24 @@ void unit::loseHp(unit const *attacker){
 
 unit* unit::parseUnit(std::string fname){
         std::ifstream f(fname);   
-        std::string t,n;
-        double h,d;
+        std::string t;
+        std::string n = "";
+        double h = -1;
+        double d = -1;
         if (!f) throw fname+" file does not exist!" ;
-        std::getline(f,t);  //skip first line
-        
-        std::getline(f,t);  //read name line
-        t = t.substr(t.find(": \"")+3); //trim string
-        n = t.substr(0,t.size()-2);  //further trim string and make name equal
-
-        std::getline(f,t);  //read hp line
-        t = t.substr(t.find(": ")+2);
-        h = std::stod(t);
-
-        std::getline(f,t);  //read dmg line
-        t = t.substr(t.find(": ")+2);
-        d = std::stod(t);
-
+        while (!f.eof()) {
+                std::getline(f,t);
+                if((t.find("name") != std::string::npos) && n == ""){
+                        t = t.substr(t.find(": \"")+3); //trim string
+                        n = t.substr(0,t.size()-2);  //further trim string and make name equal
+                }else if((t.find("hp") != std::string::npos) && h == -1){
+                        t = t.substr(t.find(": ")+2);
+                        h = std::stod(t);
+                }else if((t.find("dmg") != std::string::npos) && d == -1){
+                        t = t.substr(t.find(": ")+2);
+                        d = std::stod(t);
+                }
+        }
         f.close();
         return new unit(n,h,d);
 }
