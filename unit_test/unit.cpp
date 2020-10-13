ad0bb43d01f78caa168ebc3f3f77b005ecbef0aa
vbence121 <vbence121@gmail.com>
2020-10-13 19:12:41 +0200

    Add unit tests

---
+++ unit_test/unit.cpp
@@ -0,0 +1,38 @@
+#include <map>
+#include <iterator>
+#include <string>
+#include <fstream>
+#include "unit.h"
+#include "jsonparser.h"
+
+//  getter fügvények
+double unit::getHp () const { return hp; }
+double unit::getDmg () const { return dmg; }
+std::string unit::getName () const { return name; }
+
+bool unit::isAlive () const { return (hp>0) ? true : false; }
+
+void unit::loseHp(unit const *attacker){
+        hp -= attacker->getDmg();
+        if (hp<0) hp=0;
+    }
+
+unit* unit::parseUnit(std::string fname){
+        std::string n;
+        double d,h;
+        std::map<std::string,std::string> m;
+        try{
+            m = jsonparser::fileInp(fname);
+        }catch(const std::string e){
+                std::cerr << e << '\n';
+                //std::exit( -1);
+        }        
+        std::map<std::string, std::string>::iterator itr;
+        for (itr = m.begin(); itr != m.end(); ++itr) {
+                if(itr->first == "name") n = jsonparser::rFVbQ(itr->second);
+                else if(itr->first == "dmg") d = stod(itr->second);
+                else if(itr->first == "hp") h = stod(itr->second);
+                else continue;
+        }
+        return new unit(n,h,d);
+}
---
+++ unit_test/unittest.cpp
@@ -0,0 +1,43 @@
+#include "../unit.h"
+#include "../jsonparser.h"
+
+#include <string>
+#include <map>
+#include <gtest/gtest.h>
+
+TEST(Multest, resAsExpected){
+    unit expected("TestUnit", 64, 32);
+    unit* result = unit::parseUnit("unit2test.json");
+
+    EXPECT_EQ(expected.getName(), result->getName());
+    EXPECT_DOUBLE_EQ(expected.getDmg(), result->getDmg());
+    EXPECT_DOUBLE_EQ(expected.getHp(), result->getHp());
+};
+TEST(Multest, allElementsFilled){
+    unit* result = unit::parseUnit("unit2test.json");
+
+    EXPECT_NE("", result->getName());
+    EXPECT_NE(NULL, result->getDmg());
+    EXPECT_NE(NULL, result->getHp());
+};
+TEST(Multest, fileException){
+    unit expected("TestUnit", 64, 32);
+    std::string fname = "unit2test.jso";
+    const std::string exp = fname+" file does not exist!" ;
+    
+        try
+        {
+            unit* result = unit::parseUnit(fname);
+        }
+        catch(const std::string e)
+        {
+            EXPECT_STREQ(exp.c_str(), e.c_str());
+            //throw;
+        }
+};
+
+
+int main(){
+    ::testing::InitGoogleTest();
+    return RUN_ALL_TESTS();
+}
