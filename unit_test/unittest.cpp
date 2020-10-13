bb3a149ba21ac424648d6c576ff37ababbc1aaaf
vbence121 <vbence121@gmail.com>
2020-10-13 19:38:42 +0200

    fix unittest.cpp (I swear it works on my pc)

--- unit_test/unittest.cpp
+++ unit_test/unittest.cpp
@@ -37,7 +37,7 @@ TEST(Multest, fileException){
 };
 
 
-int main(){
-    ::testing::InitGoogleTest();
+int main(int argc, char **argv){
+    ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
