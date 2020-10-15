cc3857c0b5de9dd6af06a2325e96976dd81c463a
VMatthew <vmate0116@gmail.com>
2020-10-15 17:30:15 +0200

    Revert "Implement player class"
    This reverts commit 3d1352f9a71b9b3ecc475d246bb53dd305687770.

--- main.cpp
+++ main.cpp
@@ -5,34 +5,36 @@
 
 using namespace std;
 
-bool battle(unit* const u1, unit* u2) {
+bool battle(unit* const u1, unit* u2){ 
     u2->loseHp(u1);
     return (u2->isAlive()) ? true : false; // if defender is dead, return false
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]){
 
     vector<unit*> alive;
     vector<unit*> dead;
-    try
-    {
-        alive.push_back(player::parsePlayer(argv[1]));
-        alive.push_back(unit::parseUnit(argv[2]));
-    }
-    catch (const string e)
-    {
-        std::cerr << e << '\n';
-        for (auto a : alive) delete a;
-        for (auto d : dead) delete d;
-        return -1;
-    }
+try
+{
+    alive.push_back(unit::parseUnit(argv[1]));
+    alive.push_back(unit::parseUnit(argv[2]));
+}
+catch(const string e)
+{
+    std::cerr << e << '\n';
+    for (auto a: alive) delete a;
+    for (auto d: dead) delete d;
+    return -1;
+}
 
     unit* attacker = alive[0];
     unit* defender = alive[1];
     while (alive.size() > 1) {
-        if (!(battle(attacker, defender))) {
+        if (!(battle(attacker,defender))){
             cout << attacker->getName() << " wins. Remaining HP: " << attacker->getHp() << endl;
-            dead.push_back(defender);
+            //deleting defender var caused segmentation fault, instead alive's size is being reduced by one and defender is being added to the "dead" vector
+			dead.push_back(defender);
+			//alive.resize(alive.size()-1);
             auto it = std::find(alive.begin(), alive.end(), defender);
             if (it != alive.end()) { alive.erase(it); }
             continue;
@@ -41,6 +43,6 @@ int main(int argc, char* argv[]) {
         attacker = defender;
         defender = temp;
     }
-    for (auto a : alive) delete a;
-    for (auto d : dead) delete d;
+    for (auto a: alive) delete a;
+    for (auto d: dead) delete d;
 }
--- unit.h
+++ unit.h
@@ -7,37 +7,19 @@
 class unit {
 private:
     const std::string name;
-    double hp;
+    double hp;  
     const double  dmg; // health point, damage point
 public:
-    unit(std::string name, double hp, double dmg) : name(name), hp(hp), dmg(dmg) {}
+    unit(std::string name, double hp, double dmg): name(name), hp(hp), dmg(dmg) {}
 
-    //  getter f�ggv�nyek
+    //  getter fügvények
     double getHp() const;
     double getDmg() const;
-    std::string getName() const;
+    std::string getName() const ;
 
     bool isAlive() const;
 
-    virtual void loseHp(unit* attacker);
-    virtual double dealDamage() { return getDmg(); }
+    void loseHp(unit const *attacker);
+
     static unit* parseUnit(std::string fname);
-};
-
-class player : public unit {
-private:
-    const std::string name;
-    double hp, dmg, exp, lvl; // health point, damage point, experience, level
-public:
-    player(std::string name, double hp, double dmg, double exp, double lvl) : unit(name, hp, dmg), exp(exp), lvl(lvl) {}
-
-    //getter f�ggv�nyek
-    double getExp() const;
-    double getLvl() const;
-
-    void gainXP(unit const* attacker);
-
-    static player* parsePlayer(std::string fname);
-    double dealDamage();
-    //void loseHp(unit const* attacker); // re-def
 };
